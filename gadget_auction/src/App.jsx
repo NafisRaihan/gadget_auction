9d0e0c82338c9bce021cbc550004ee2fc5d75086
Nafis Raihan <nafisraihan@Nafiss-MacBook-Pro.local>
2025-10-30 00:23:05 +0600

    msfsd

---
+++ gadget_auction/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>haha now its under my control</h1>
+    </>
+  )
+}
+
+export default App
